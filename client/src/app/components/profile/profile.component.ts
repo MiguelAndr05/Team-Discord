f3e18b8b9fbc912ff27dcf02adabaa993f21ee72
Adamdijulio <adamdijulio@gmail.com>
2025-03-31 19:02:27 -0400

    profile page layout01 with friends list and message box

--- client/src/app/components/profile/profile.component.ts
+++ client/src/app/components/profile/profile.component.ts
@@ -12,6 +12,10 @@ export class ProfileComponent {
   public inputMessage: string = "";
   //String array to hold messages between users
   public messageBox: string[] = [];
+  //String array to hold friends
+  public friendsList: string[] = ["Mario", "Luigi", "Bowser"];
+  //String array to hold friend requests
+  public friendRequest: string[] = [];
   
   //Method to send message after clicking a button
   sendMessage(){
