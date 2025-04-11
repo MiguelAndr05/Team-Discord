f7d0d5a0b957c79c03ab0d41a5806c4cfacd24f4
Adamdijulio <adamdijulio@gmail.com>
2025-04-11 04:45:36 -0400

    routing

--- client/src/app/auth.service.ts
+++ client/src/app/auth.service.ts
@@ -6,7 +6,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:5000';
+  // private apiUrl = 'http://localhost:5000';
+  private apiUrl = 'https://team-discord-server.onrender.com';
 
   constructor(private http: HttpClient) {}
 
